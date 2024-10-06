71cf63957c5e359ad9c3c8a385aa67ab70504175
muhammadahmadazam <139275803+muhammadahmadazam@users.noreply.github.com>
2024-10-06 16:25:59 +0500

    Completed 3a and 3b

--- components/hero.js
+++ components/hero.js
@@ -3,25 +3,31 @@ import React from 'react';
 
 const ContentPictureComponent = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center lg:items-start p-4">
+    <div className="flex flex-col lg:flex-row lg:gap-8 items-center lg:items-center p-4 lg:h-screen px-[1.5rem] mx-auto max-w-[1450px]">
       {/* Content Section */}
-      <div className="lg:w-1/2 p-4 mt-24 flex flex-col justify-center text-center">
-      <h1 className="font-montserrat typing-animation mb-4 font-bold text-[#3b445f] lg:text-2xl mt-16 " style={{ fontSize: "2.4rem" }}>
-      Wise Numbers LTD
-    </h1>
-        <p className="text-base lg:text-lg text-gray-700 mt-12">
-  Wise Numbers LTD, led by Farhan Butt, is a family-oriented firm with extensive experience in the UK. The firm provides expert financial guidance tailored to individual client needs. Supported by a skilled team of bookkeepers, accountants, and tax experts, Wise Numbers caters to a diverse clientele, from freelancers to large organizations. Their services include strategic financial consultancy and advanced tax planning, making them a trusted partner for achieving financial success and peace of mind.
-</p>
-
+      <div className="lg:w-1/2 p-4 mt-24 flex flex-col justify-center text-center lg:mt-0 lg:p-0">
+        <h1 className="font-montserrat mb-4 font-bold text-gray-700 text-2xl mt-4 sm:text-4xl lg:mt-16 ">
+          Wise Numbers LTD
+        </h1>
+        <p className="text-base lg:text-lg text-gray-700 mt-4 md:mt-12">
+          Wise Numbers LTD, is an Expert-Led Accounting Firm, with extensive
+          experience in the UK. The firm provides tailored financial services to
+          varying client needs. Supported by a skilled team of bookkeepers,
+          accountants, and tax experts, Wise Numbers caters to a diverse
+          clientele, from freelancers to large organizations. Their services
+          include strategic financial consultancy and advanced tax planning,
+          making them a trusted partner for achieving financial success and
+          peace of mind.
+        </p>
       </div>
-      
+
       {/* Picture Section */}
-      <div className="lg:w-1/2 p-4 mt-8 sm:mt-16 lg:mt-16 flex justify-center">
+      <div className="lg:w-1/2 p-4 mt-8 sm:mt-16 lg:mt-16 lg:p-0 flex justify-center">
         <img
-          src="/gb.svg"
-          alt="Description of Image"
-          className="object-cover rounded-lg w-full max-w-md lg:max-w-full"
-          style={{ height: "auto" }} // Ensure height is auto to maintain aspect ratio
+          src="/heroSection.png"
+          alt="Hero section Image"
+          className="rounded-lg w-full  max-w-[320px] sm:max-w-[451px] "
+          style={{ height: 'auto' }} // Ensure height is auto to maintain aspect ratio
         />
       </div>
     </div>
--- pages/test.js
+++ pages/test.js
@@ -7,33 +7,34 @@ import Logo from '../components/logo';
 import Talk from '../components/talk';
 import GetIn from '../components/getIn';
 const Business = () => {
-    return (
-        <div>
+  return (
+    <div>
       <Navbar />
-      
-        <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/back.jpg")' }}>
-            <div className="absolute inset-0 bg-gray-900 opacity-40"></div>
-            <div className="relative h-full flex flex-col justify-center items-center">
-                <div className="max-w-3xl mx-auto px-6 text-center">
-                    <h1 className="text-white text-4xl md:text-6xl font-bold animate-bounce">
-                        Business Strategy & Solutions
-                    </h1>
-                    <p className="text-white mt-4 mb-8 text-lg md:text-2xl">
-                        Experience the Best Services with Us
-                    </p>
-                    
-                </div>
-            </div>
-            <ServicesContent/>
-            <Logo/>
-<Services/>
-<GetIn/>
-<Talk/>
-<Footer/>
+
+      <div
+        className="relative bg-cover bg-center h-screen"
+        style={{ backgroundImage: 'url("/back.jpg")' }}
+      >
+        <div className="absolute inset-0 bg-gray-900 opacity-40"></div>
+        <div className="relative h-full flex flex-col justify-center items-center">
+          <div className="max-w-3xl mx-auto px-6 text-center">
+            <h1 className="text-white text-4xl md:text-6xl font-bold animate-bounce">
+              Business Strategy & Solutions
+            </h1>
+            <p className="text-white mt-4 mb-8 text-lg md:text-2xl">
+              Experience the Best Services with Us
+            </p>
+          </div>
         </div>
-       
-        </div>
-    );
+        <ServicesContent />
+        <Logo />
+        <Services />
+        <GetIn />
+        <Talk />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Business;
